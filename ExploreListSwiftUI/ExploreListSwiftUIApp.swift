66ddd63d0e7a9d54e5c1f4dd3d4290fb20e62e80
dahdouhMouad <mouaddhd10@gmail.com>
2023-11-08 14:31:15 +0100

    Initial Commit

---
+++ ExploreListSwiftUI/ExploreListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreListSwiftUIApp.swift
+//  ExploreListSwiftUI
+//
+//  Created by Macbook Pro on 8/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExploreListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
